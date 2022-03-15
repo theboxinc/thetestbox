5c0f4cd3b472da3ea2a6249bc71a4980b9e5f7d8
TheBoxInc2 <101322449+TheBoxInc2@users.noreply.github.com>
2022-03-15 21:40:50 +0100

    Add files via upload

---
+++ verarbeitung.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8" />
+  <title>Beispielformular</title>
+</head>
+<body>
+<form action="verarbeitung.php" method="get">
+Ihr Vorname: <br />
+<input type="text" name="vorname" size="20" maxlength="30" />
+<br />
+Ihr Nachname: <br />
+<input type="text" name="nachname" size="20" maxlength="30" /><br />
+E-Mail: <br />
+<input type="text" name="email" size="20" maxlength="30" />
+<br />
+Telefon: <br />
+<input type="text" name="telefon" size="20" maxlength="30" />
+<br />
+<input type="submit" value="Abschicken" />
+</form>
+
+</body>
+</html>
